afce64e384f8c53da8e5cc7ac5bc29eef9ae8d73
unknown <eldergsilva@gmail.com>
2024-03-26 10:01:41 -0300

    adicionado a funcionalidade de alterar foco do botão de acordo com o click

--- Fokus-projeto-base/scrips.js
+++ Fokus-projeto-base/scrips.js
@@ -1,21 +1,66 @@
 const html =document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt =document.querySelector('.app__card-button--curto');
+
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 
+ 
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
+
+const botaoIniciar = document.querySelector('.app__card-primary-button');
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
+
+const botoes = document.querySelectorAll('.app__card-button')
+
+
 focoBt.addEventListener('click',()=>{
-    html.setAttribute('data-contexto','foco')
+   alterarContexto('foco')
+   focoBt.classList.add('active')
 })
 curtoBt.addEventListener('click',()=>{
-   html.setAttribute('data-contexto','descanso-curto')
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click',()=>{''
-    html.setAttribute('data-contexto','descanso-longo')
-    
+    alterarContexto('descanso-longo')  
+    longoBt.classList.add('active')  
 })
 
-// O documento HTML, onde serão manipulados os elementos; DOM 
-// O elemento HTML, em que irá aparecer o temporizador; CARD
-// O elemento HTML, no qual as imagens de cada contexto irão trocar de posição;
-// O elemento HTML, onde as frases de cada contexto irão trocar de posição.Vamos lá? H1
+
+function  alterarContexto (contexto){
+    botoes.forEach(
+        function (contexto){
+            contexto.classList.remove('active')
+        }
+    )
+   html.setAttribute('data-contexto',contexto)
+   banner.setAttribute('src',`./imagens/${contexto}.png`)
+   switch (contexto) {
+    case "foco":
+        titulo.innerHTML = `Otimize sua produtividade,<br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong>`   
+        break;
+    case "descanso-curto" : 
+        titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+        break;
+    case "descanso-longo" :
+        titulo.innerHTML = `Hora de voltar à superfície.<strong class="app__title-strong">
+        Faça uma pausa longa.</strong>`
+
+    default:
+        break;
+   }
+
+
+}
+document.querySelector('html');
+document.querySelector('.app__card-primary-button');
+document.querySelector('#timer');
+document.querySelector('.app__image');
+document.querySelector('.app__title'); 
+
