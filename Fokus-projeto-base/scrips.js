051917aa17a6cdfbc8930fa8d4b61c7e60def1d0
unknown <eldergsilva@gmail.com>
2024-03-26 13:20:05 -0300

    adicionado a funcionalidade de alterar icone começar/pausar

--- Fokus-projeto-base/scrips.js
+++ Fokus-projeto-base/scrips.js
@@ -1,19 +1,25 @@
-const html =document.querySelector('html');
+const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo') 
-const displayTempo = document.querySelector('#timer');
-const banner = document.querySelector('.app__image');
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
-const startPauseBt =querySelector('#start-pause') 
-
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
-const musica = new Audio ('./sons/luna-rise-part-one.mp3')
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+
+
+const audioPlay = new Audio('./sons/play.wav');
+const audioPausa = new Audio('./sons/pause.mp3')
+const audioTempofinalizado= new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 
 musica.loop =true;
-
 musicaFocoInput.addEventListener('change',()=>{
     if (musica.paused) {
         musica.play()               
@@ -22,35 +28,26 @@ musicaFocoInput.addEventListener('change',()=>{
     }
 })
 
-const botaoIniciar = document.querySelector('.app__card-primary-button');
-const duracaoFoco = 1500; 
-const duracaoDescansoCurto = 300; 
-const duracaoDescansoLongo = 900; 
-
-const botoes = document.querySelectorAll('.app__card-button')
-
-
 focoBt.addEventListener('click',()=>{
    alterarContexto('foco')
    focoBt.classList.add('active')
 })
+
 curtoBt.addEventListener('click',()=>{
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
-longoBt.addEventListener('click',()=>{''
+longoBt.addEventListener('click',()=>{ 
     alterarContexto('descanso-longo')  
     longoBt.classList.add('active')  
 })
 
 
 function  alterarContexto (contexto){
-    botoes.forEach(
-        function (contexto){
-            contexto.classList.remove('active')
-        }
-    )
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+   })
    html.setAttribute('data-contexto',contexto)
    banner.setAttribute('src',`./imagens/${contexto}.png`)
    switch (contexto) {
@@ -68,16 +65,34 @@ function  alterarContexto (contexto){
     default:
         break;
    }
-
-
 }
-document.querySelector('html');
-document.querySelector('.app__card-primary-button');
-document.querySelector('#timer');
-document.querySelector('.app__image');
-document.querySelector('.app__title'); 
 
 const contagemRegressiva = () =>{
-    tempoDecorridoEmSegundos -=1
-    console.log('temporizador:'+tempoDecorridoEmSegundos);
+    if (tempoDecorridoEmSegundos < 0) {
+        zerar()
+        alert('tempo finalizado ')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador:' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click',iniciarOuPausar)
+
+function iniciarOuPausar (){
+    if (intervaloId) {
+    audioPausa.play()
+        zerar()
+        return        
+    }
+    audioPlay.play()
+    intervaloId =setInterval(contagemRegressiva,1000)
+    iniciarOuPausarBt.textContent="Pausar"
+    
+}
+
+function zerar (){
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent="Começar"
+    intervaloId=null    
 }
